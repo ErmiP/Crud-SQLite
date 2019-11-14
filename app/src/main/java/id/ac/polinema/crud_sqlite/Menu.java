578fe8edeb72ec096045e704b3b19567183f1678
ErmiP <ermi.pristiya@gmail.com>
2019-11-14 15:24:40 +0700

    Crud-Sqlite ver 1

--- app/src/main/java/id/ac/polinema/crud_sqlite/Menu.java
+++ app/src/main/java/id/ac/polinema/crud_sqlite/Menu.java
@@ -7,14 +7,17 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class Menu Activity implements OnClickListener{
+public class Menu extends Activity implements OnClickListener{
     private Button bTambah;
+    private Button bLihat;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
         bTambah = (Button) findViewById(R.id.button_tambah);
         bTambah.setOnClickListener(this);
+        bLihat = (Button) findViewById(R.id.button_view);
+        bLihat.setOnClickListener(this);
     }
     @Override
     public void onClick(View v) {
@@ -24,6 +27,10 @@ public class Menu Activity implements OnClickListener{
             case R.id.button_tambah :
                 Intent i = new Intent(this, CreateData.class);
                 startActivity(i);
+                break;
+            case R.id.button_view :
+                Intent i2 = new Intent(this, ViewData.class);
+                startActivity(i2);
                 break;
         }
     }
